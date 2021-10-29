df618c041de92cd1f0e6b4c7ba584ac005dc2e9f
umamahale <umagasmos@gmail.com>
2021-10-29 09:16:10 -0400

    first commit

---
+++ EmployeeManagement/EmployeeManagement/SalaryUpdateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement
+{
+    class SalaryUpdateModel
+    {
+        public int SalaryId { get; set; }
+        public string Month { get; set; }
+        public int EmployeeSalary { get; set; }
+        public int EmployeeId { get; set; }
+
+    }
+}
